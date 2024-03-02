19a78ba6c948bde1becf7bedbadb62c59bc3d5d2
paul <pmolch3.1415@gmail.com>
2024-03-02 19:17:04 +0300

    update vue 3.21 to 3.41 & create name input

---
+++ resources/js/Shared/NameInput.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { FwbInput } from 'flowbite-vue'
+import InputLabel from '@/Shared/InputLabel.vue'
+
+const [model] = defineModel({
+  set(value) {
+    return value
+      .split(' ')
+      .map((word) => (word.length === 1 ? word : word.charAt(0).toUpperCase() + word.slice(1)))
+      .join(' ')
+  },
+})
+
+defineProps({
+  label: {
+    type: String,
+    default: 'Название',
+  },
+  errorMessage: String,
+})
+</script>
+
+<template>
+  <InputLabel :value="label">
+    <FwbInput v-model="model" :validation-status="errorMessage ? 'error' : ''">
+      <template #validationMessage v-text="errorMessage" />
+    </FwbInput>
+  </InputLabel>
+</template>
+
+<style scoped></style>
