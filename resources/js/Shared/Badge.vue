01c3e0a1b030f61b9ddd0c7f5565656fea35c86c
paul <pmolch3.1415@gmail.com>
2024-07-04 19:00:31 +0300

    fix badge types

--- resources/js/Shared/Badge.vue
+++ resources/js/Shared/Badge.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { FwbBadge } from 'flowbite-vue'
 
-type FwbBadgeSize = typeof FwbBadge.__defaults.size | 'lg'
+type FwbBadgeSize = typeof FwbBadge.__defaults.size
 type FwbBadgeType = typeof FwbBadge.__defaults.type
 
 withDefaults(
   defineProps<{
     icon?: string
     closable?: boolean
-    size?: FwbBadgeSize
+    size?: FwbBadgeSize | 'lg'
     theme?: FwbBadgeType
   }>(),
   {
@@ -21,7 +21,11 @@ defineEmits(['close'])
 </script>
 
 <template>
-  <FwbBadge :type="theme" :size="size" class="gap-1.5 whitespace-nowrap items-center">
+  <FwbBadge
+    :type="theme"
+    :size="size as FwbBadgeSize"
+    class="gap-1.5 whitespace-nowrap items-center"
+  >
     <template v-if="icon" #icon>
       <Icon :icon="['fas', icon]" size="sm" />
     </template>
